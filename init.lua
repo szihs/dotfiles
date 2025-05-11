cc97367adb931d49ac8e806b62efd90d34021aeb
Harsh Aggarwal <harsh1492@gmail.com>
2025-05-11 20:43:57 +0530

    :chechealth nvim-treesitter :echo nvim_list_runtime_paths() :lua =vim.opt :e std_path("data")

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+vim.opt.shiftwidth = 4
+-- press p to paste clip board buffer
+vim.opt.clipboard = "unnamedplus"
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
