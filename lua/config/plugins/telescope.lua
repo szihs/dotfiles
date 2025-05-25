bfc16a49644d10fe65e7247fdb2418e87a309ddf
Harsh Aggarwal <harsh1492@gmail.com>
2025-05-25 21:27:25 +0530

    multigrep;

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -12,16 +12,32 @@ return {
           find_files = {
             theme = "ivy"
           }
+        },
+        extensions = {
+          fzf = {}
         }
       }
+
+      require('telescope').load_extension('fzf')
+
       vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
       vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+
       vim.keymap.set("n", "<space>en", function()
         local opts = require('telescope.themes').get_dropdown({
           cwd = vim.fn.stdpath("config")
         })
         require('telescope.builtin').find_files(opts)
       end)
+
+      vim.keymap.set("n", "<space>ep", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+
+
+      require("config.telescope.multigrep").setup()
     end
   }
 }
