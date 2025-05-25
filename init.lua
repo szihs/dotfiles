0edb472aa8192a9edaa5d3b5f12487f4d96de6af
Harsh Aggarwal <harsh1492@gmail.com>
2025-05-25 16:24:45 +0530

    lsp: autoformatting and .editorconfig

--- init.lua
+++ init.lua
@@ -39,9 +39,20 @@ vim.o.winborder = 'rounded'
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then return end
+
     if client:supports_method('textDocument/completion') then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
     end
+
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+      and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+	buffer = ev.buf,
+	callback = function()
+	  vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+	end,
+      })
+    end
   end,
 })
-
