7b00bf1751f9a7d5cc8f699a2df39595234dba1b
Harsh Aggarwal <harsh1492@gmail.com>
2025-06-07 15:04:10 +0530

    Ctrl-Q, Alt J/K, _ cc, _ st

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("config.lazy")
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 -- vim.cmd [[ hi @function.builtin.lua guifg=pink]]
 
@@ -11,22 +13,43 @@ vim.opt.clipboard = "unnamedplus"
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Terminal Bindings',
+  group = vim.api.nvim_create_augroup('custom-terminal', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
 
 
+local job_id = 0
+vim.keymap.set("n", "<space>cc", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+
+  job_id = vim.bo.channel
+end)
+
+vim.keymap.set("n", "<space>st", function()
+  vim.fn.chansend(job_id, { "claude \r\n" })
+end)
 
 -- vim.lsp.enable({'luals', 'clangd'})
 local lsp_configs = {}
 for _, f in pairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
-	local server_name = vim.fn.fnamemodify(f, ':t:r')
-	table.insert(lsp_configs, server_name)
+  local server_name = vim.fn.fnamemodify(f, ':t:r')
+  table.insert(lsp_configs, server_name)
 end
 
 vim.lsp.enable(lsp_configs)
@@ -37,22 +60,22 @@ vim.diagnostic.config({ virtual_text = { current_line = true } })
 vim.o.winborder = 'none'
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if not client then return end
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then return end
 
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
 
-		if not client:supports_method('textDocument/willSaveWaitUntil')
-				and client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				buffer = ev.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
-		end
-	end,
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
 })
