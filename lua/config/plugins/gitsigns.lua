00df1a44cf44028a6b3eaaa5bd61ef08ca621e90
Harsh Aggarwal <haaggarwal@nvidia.com>
2025-06-09 14:44:53 +0530

    working

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -1,5 +1,6 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
+  'tpope/vim-fugitive',
   'lewis6991/gitsigns.nvim',
   opts = {
     -- See `:help gitsigns.txt`
