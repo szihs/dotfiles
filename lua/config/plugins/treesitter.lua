7e1db2fe899b0fd0c09b85527d604ae99603d73b
Harsh Aggarwal <haaggarwal@nvidia.com>
2025-06-24 10:30:45 +0530

    add tree-sitter-slang and rendermarkdown

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript" },
+        ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "slang" },
         auto_install = true,
         highlight = {
           enable = true,
